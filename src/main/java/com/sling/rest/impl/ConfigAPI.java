dcd2cd98d424f61c3830e1b3dc794051b04398da
zeevs <shalev.zeev@gmail.com>
2016-08-18 15:29:44 +0300

    first commit

---
+++ src/main/java/com/sling/rest/impl/ConfigAPI.java
@@ -0,0 +1,256 @@
+package com.sling.rest.impl;
+
+import java.io.InputStream;
+import java.util.List;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
+import javax.mail.BodyPart;
+import javax.mail.internet.MimeMultipart;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.folio.rulez.Rules;
+import com.sling.rest.annotations.Validate;
+import com.sling.rest.jaxrs.model.Config;
+import com.sling.rest.jaxrs.model.Configs;
+import com.sling.rest.jaxrs.resource.ConfigurationsResource;
+import com.sling.rest.persist.MongoCRUD;
+import com.sling.rest.resource.utils.OutStream;
+import com.sling.rest.resource.utils.RestUtils;
+import com.sling.rest.tools.Messages;
+
+@Path("apis/configurations")
+public class ConfigAPI implements ConfigurationsResource {
+
+  private final Messages            messages = Messages.getInstance();
+  private static final ObjectMapper mapper   = new ObjectMapper();
+  public static final String CONFIG_COLLECTION  = "config_data";
+  public static final String METHOD_GET         = "get";
+  public static final String METHOD_POST        = "post";
+  public static final String METHOD_PUT         = "put";
+  public static final String METHOD_DELETE      = "delete";
+
+  @Validate
+  @Override
+  public void getConfigurationsTables(String authorization, String query, String orderBy, Order order, int offset, int limit, String lang,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context context) throws Exception {
+
+    try {
+      System.out.println("sending... getConfigurationsTables");
+      context.runOnContext(v -> {
+        MongoCRUD.getInstance(context.owner()).get(
+          MongoCRUD.buildJson(Config.class.getName(), CONFIG_COLLECTION, query, orderBy, order, offset, limit),
+            reply -> {
+              try {
+                Configs configs = new Configs();
+                List<Config> config = (List<Config>)reply.result();
+                configs.setConfigs(config);
+                configs.setTotalRecords(config.size());
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetConfigurationsTablesResponse.withJsonOK(configs)));
+              } catch (Exception e) {
+                e.printStackTrace();
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetConfigurationsTablesResponse
+                    .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+              }
+            });
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetConfigurationsTablesResponse.withPlainInternalServerError(messages
+          .getMessage(lang, "10001"))));
+    }
+
+  }
+
+  @Validate
+  @Override
+  public void postConfigurationsTables(String authorization, String lang, Config entity, Handler<AsyncResult<Response>> asyncResultHandler,
+      Context context) throws Exception {
+
+    try {
+      System.out.println("sending... postConfigurationsTables");
+      JsonObject jObj = RestUtils.createMongoObject(CONFIG_COLLECTION, METHOD_POST, authorization, null, null, null, 0, 0,
+          entity, null);
+
+      context.runOnContext(v -> {
+        MongoCRUD.getInstance(context.owner()).save(CONFIG_COLLECTION, entity,
+            reply -> {
+              try {
+                Object ret = reply.result();
+                OutStream stream = new OutStream();
+                stream.setData(ret);
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PostConfigurationsTablesResponse.withJsonCreated(stream
+                    .getData().toString(), stream)));
+              } catch (Exception e) {
+                e.printStackTrace();
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PostConfigurationsTablesResponse
+                    .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+              }
+            });
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PostConfigurationsTablesResponse.withPlainInternalServerError(messages
+          .getMessage(lang, "10001"))));
+    }
+  }
+
+  @Validate
+  @Override
+  public void getConfigurationsTablesByTableId(String tableId, String authorization, String query, String orderBy, Order order, int offset,
+      int limit, String lang, Handler<AsyncResult<Response>> asyncResultHandler, Context context) throws Exception {
+
+    try {   
+      System.out.println("sending... getConfigurationsTablesByTableId");
+      context.runOnContext(v -> {
+        JsonObject q = new JsonObject();
+        if(query != null){
+          q = new JsonObject(query);          
+        }
+        q.put("_id", tableId);
+        MongoCRUD.getInstance(context.owner()).get(
+          MongoCRUD.buildJson(Config.class.getName(), CONFIG_COLLECTION, q, orderBy, order, offset, limit),
+            reply -> {
+              try {
+                Object ret = reply.result();
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetConfigurationsTablesByTableIdResponse
+                    .withJsonOK(new ObjectMapper().readValue(ret.toString(), Configs.class))));
+              } catch (Exception e) {
+                e.printStackTrace();
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetConfigurationsTablesByTableIdResponse
+                    .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+              }
+            });
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetConfigurationsTablesByTableIdResponse
+          .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+    }
+
+  }
+
+  @Validate
+  @Override
+  public void deleteConfigurationsTablesByTableId(String tableId, String authorization, String lang,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context context) throws Exception {
+
+    try {
+      JsonObject q = new JsonObject();
+      q.put("_id", tableId);
+      System.out.println("sending... deleteConfigurationsTablesByTableId");
+
+      context.runOnContext(v -> {
+        MongoCRUD.getInstance(context.owner())
+            .delete(CONFIG_COLLECTION, q,
+                reply -> {
+                  try {
+                    Object ret = reply.result();
+                    asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(DeleteConfigurationsTablesByTableIdResponse
+                        .withNoContent()));
+                  } catch (Exception e) {
+                    e.printStackTrace();
+                    asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(DeleteConfigurationsTablesByTableIdResponse
+                        .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+                  }
+                });
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(DeleteConfigurationsTablesByTableIdResponse
+          .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+    }
+
+  }
+
+  @Validate
+  @Override
+  public void putConfigurationsTablesByTableId(String tableId, String authorization, String lang, Configs entity,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context context) throws Exception {
+
+    try {
+      JsonObject q = new JsonObject();
+      q.put("_id", tableId);
+      System.out.println("sending... putConfigurationsTablesByTableId");
+
+      context.runOnContext(v -> {
+        MongoCRUD.getInstance(context.owner()).update(CONFIG_COLLECTION, entity, q,
+            reply -> {
+              try {
+                Object ret = reply.result();
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutConfigurationsTablesByTableIdResponse.withNoContent()));
+              } catch (Exception e) {
+                e.printStackTrace();
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutConfigurationsTablesByTableIdResponse
+                    .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+              }
+            });
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutConfigurationsTablesByTableIdResponse
+          .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+    }
+
+  }
+
+
+  @Validate
+  @Override
+  public void postConfigurationsRules(String authorization, String lang, MimeMultipart entity,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+
+    try {
+
+      System.out.println("sending... postConfigurationsRules");
+      int parts = entity.getCount();
+      Object drool = null;
+      Config conf = null;
+      for (int i = 0; i < parts; i++) {
+        BodyPart part = entity.getBodyPart(i);
+        //check header for type - not useful right now -needs to be forwarded by the framework via the mimemultipart object (not supported yet)
+        //part.getContentType().contains("text/plain");
+        InputStream rule = part.getInputStream();
+        if(Rules.validateRules(rule).size() ==0){
+          //this is the rules
+          drool = part.getContent();
+        }
+        else {
+          //this is the config entry - load into a config object, if that fails, data passed is not correct
+          conf = mapper.readValue(part.getContent().toString(), Config.class);
+        }
+      }
+      conf.getRows().get(0).setValue(drool.toString());
+      final Config fconf = conf;
+      vertxContext.runOnContext(v -> {
+        MongoCRUD.getInstance(vertxContext.owner()).save(CONFIG_COLLECTION, fconf,
+            reply -> {
+              try {
+                Object ret = reply.result();
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutConfigurationsTablesByTableIdResponse.withNoContent()));
+              } catch (Exception e) {
+                e.printStackTrace();
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutConfigurationsTablesByTableIdResponse
+                    .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+              }
+            });
+      });
+    } catch (Exception e) {
+      e.printStackTrace();
+      asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutConfigurationsTablesByTableIdResponse
+          .withPlainInternalServerError(messages.getMessage(lang, "10001"))));
+    }
+    
+  }
+
+  @Validate
+  @Override
+  public void getConfigurationsRules(String authorization, String lang, Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) throws Exception {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
---
+++ src/test/java/com/sling/rest/RestVerticleTest.java
@@ -0,0 +1,164 @@
+package com.sling.rest;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.common.io.ByteStreams;
+
+/**
+ * This is our JUnit test for our verticle. The test uses vertx-unit, so we declare a custom runner.
+ */
+@RunWith(VertxUnitRunner.class)
+public class RestVerticleTest {
+
+  private Vertx             vertx;
+  private ArrayList<String> urls;
+
+  /**
+   * Before executing our test, need to deploy our 2 verticles - the persistence verticle to write to mongo anf then the rest verticle.
+   * <p/>
+   * 
+   * @param context
+   *          the test context.
+   */
+  @Before
+  public void setUp(TestContext context) throws IOException {
+    vertx = Vertx.vertx();
+    DeploymentOptions options = new DeploymentOptions().setConfig(new JsonObject().put("http.port",
+      Integer.valueOf(System.getProperty("http.port"))));
+    vertx.deployVerticle(RestVerticle.class.getName(), options, context.asyncAssertSuccess(id -> {
+      System.out.println("async complete =========================");
+      try {
+        urls = urlsFromFile();
+        System.out.println("url complete =========================");
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }));
+
+  }
+
+  /**
+   * This method, called after our test, just cleanup everything by closing the vert.x instance
+   *
+   * @param context
+   *          the test context
+   */
+  @After
+  public void tearDown(TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+  }
+
+  /**
+   * This method, iterates through the urls.csv and runs each url - currently only checking the returned status codes
+   *
+   * @param context
+   *          the test context
+   */
+  @Test
+  public void checkURLs(TestContext context) {
+
+    try {
+      int[] urlCount = { urls.size() };
+      // Async async = context.async(urlCount[0]);
+      urls.forEach(url -> {
+        Async async = context.async();
+        urlCount[0] = urlCount[0] - 1;
+        HttpMethod method = null;
+
+        String[] urlInfo = url.split(" , ");
+        if ("POST".equalsIgnoreCase(urlInfo[0].trim())) {
+          method = HttpMethod.POST;
+        } else if ("PUT".equalsIgnoreCase(urlInfo[0].trim())) {
+          method = HttpMethod.PUT;
+        } else if ("DELETE".equalsIgnoreCase(urlInfo[0].trim())) {
+          method = HttpMethod.DELETE;
+        } else {
+          method = HttpMethod.GET;
+        }
+        HttpClient client = vertx.createHttpClient();
+        HttpClientRequest request = client.requestAbs(method, urlInfo[1], new Handler<HttpClientResponse>() {
+
+          @Override
+          public void handle(HttpClientResponse httpClientResponse) {
+
+            System.out.println(urlInfo[1]);
+            if (httpClientResponse.statusCode() != 404) {
+              // this is cheating for now - add posts to the test case so that
+              // we dont get 404 for missing entities
+              context.assertInRange(200, httpClientResponse.statusCode(), 5);
+            }
+            // System.out.println(context.assertInRange(200, httpClientResponse.statusCode(),5).);
+            httpClientResponse.bodyHandler(new Handler<Buffer>() {
+              @Override
+              public void handle(Buffer buffer) {
+                /*
+                 * // System.out.println("Response (" // + buffer.length() // + "): ");
+                 */System.out.println(buffer.getString(0, buffer.length()));
+                async.complete();
+
+              }
+            });
+          }
+        });
+        request.headers().add("Authorization", "abcdefg");
+        request.headers().add("Accept", "application/json");
+        request.setChunked(true);
+        request.end();
+
+      });
+    } catch (Throwable e) {
+      e.printStackTrace();
+    } finally {
+
+    }
+  }
+
+  private ArrayList<String> urlsFromFile() throws IOException {
+    ArrayList<String> ret = new ArrayList<String>();
+    byte[] content = ByteStreams.toByteArray(getClass().getResourceAsStream("/urls.csv"));
+    InputStream is = null;
+    BufferedReader bfReader = null;
+    try {
+      is = new ByteArrayInputStream(content);
+      bfReader = new BufferedReader(new InputStreamReader(is));
+      String temp = null;
+      while ((temp = bfReader.readLine()) != null) {
+        ret.add(temp);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (is != null)
+          is.close();
+      } catch (Exception ex) {
+
+      }
+    }
+    return ret;
+  }
+
+}
