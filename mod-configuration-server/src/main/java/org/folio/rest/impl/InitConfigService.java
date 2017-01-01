78531a65ce9d5f618a41a782eb78fb7eae25d825
shale <shale99@users.noreply.github.com>
2017-01-01 22:10:01 +0200

    encrypt db config password, post deploy hook

---
+++ mod-configuration-server/src/main/java/org/folio/rest/impl/InitConfigService.java
@@ -0,0 +1,35 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+
+import org.folio.rest.client.AdminClient;
+import org.folio.rest.resource.interfaces.PostDeployVerticle;
+
+/**
+ * @author shale
+ *
+ */
+public class InitConfigService implements PostDeployVerticle {
+
+  @Override
+  public void init(Vertx vertx, Context context, Handler<AsyncResult<Boolean>> handler) {
+
+    System.out.println("Getting secret key to decode DB password.");
+    /** hard code the secret key for now - in production env - change this to read from a secure place */
+    String secretKey = "b2%2BS%2BX4F/NFys/0jMaEG1A";
+    int port = context.config().getInteger("http.port");
+    AdminClient ac = new AdminClient("localhost", port, null);
+    ac.postSetAESKey(secretKey, reply -> {
+      if(reply.statusCode() == 204){
+        handler.handle(io.vertx.core.Future.succeededFuture(true));
+      }
+      else{
+        handler.handle(io.vertx.core.Future.failedFuture(reply.statusCode() + ", " + reply.statusMessage()));
+      }
+    });
+  }
+
+}
