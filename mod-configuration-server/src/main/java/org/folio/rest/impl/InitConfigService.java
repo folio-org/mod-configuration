2a8dda94a6820d234012f16e883fb3fb797713ec
shale <shale99@users.noreply.github.com>
2017-01-05 21:18:46 +0200

    disale secret key until fix encoding in client generator

--- mod-configuration-server/src/main/java/org/folio/rest/impl/InitConfigService.java
+++ mod-configuration-server/src/main/java/org/folio/rest/impl/InitConfigService.java
@@ -5,7 +5,6 @@ import io.vertx.core.Context;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 
-import org.folio.rest.client.AdminClient;
 import org.folio.rest.resource.interfaces.PostDeployVerticle;
 
 /**
@@ -17,8 +16,8 @@ public class InitConfigService implements PostDeployVerticle {
   @Override
   public void init(Vertx vertx, Context context, Handler<AsyncResult<Boolean>> handler) {
 
-    System.out.println("Getting secret key to decode DB password.");
-    /** hard code the secret key for now - in production env - change this to read from a secure place */
+/*    System.out.println("Getting secret key to decode DB password.");
+    *//** hard code the secret key for now - in production env - change this to read from a secure place *//*
     String secretKey = "b2%2BS%2BX4F/NFys/0jMaEG1A";
     int port = context.config().getInteger("http.port");
     AdminClient ac = new AdminClient("localhost", port, null);
@@ -29,7 +28,9 @@ public class InitConfigService implements PostDeployVerticle {
       else{
         handler.handle(io.vertx.core.Future.failedFuture(reply.statusCode() + ", " + reply.statusMessage()));
       }
-    });
+    });*/
+    handler.handle(io.vertx.core.Future.succeededFuture(true));
+
   }
 
 }
