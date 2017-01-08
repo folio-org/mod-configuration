aa4f3af7d088a0a3f703e01908710c8338e3f764
shale <shale99@users.noreply.github.com>
2017-01-08 12:15:00 +0200

    upgrade version, change secret key in demo post deploy hook

--- mod-configuration-server/src/main/java/org/folio/rest/impl/InitConfigService.java
+++ mod-configuration-server/src/main/java/org/folio/rest/impl/InitConfigService.java
@@ -5,6 +5,7 @@ import io.vertx.core.Context;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 
+import org.folio.rest.client.AdminClient;
 import org.folio.rest.resource.interfaces.PostDeployVerticle;
 
 /**
@@ -16,9 +17,9 @@ public class InitConfigService implements PostDeployVerticle {
   @Override
   public void init(Vertx vertx, Context context, Handler<AsyncResult<Boolean>> handler) {
 
-/*    System.out.println("Getting secret key to decode DB password.");
-    *//** hard code the secret key for now - in production env - change this to read from a secure place *//*
-    String secretKey = "b2%2BS%2BX4F/NFys/0jMaEG1A";
+    System.out.println("Getting secret key to decode DB password.");
+    //** hard code the secret key  - in production env - read from a secure place *//
+    String secretKey = "b2+S+X4F/NFys/0jMaEG1A";
     int port = context.config().getInteger("http.port");
     AdminClient ac = new AdminClient("localhost", port, null);
     ac.postSetAESKey(secretKey, reply -> {
@@ -28,7 +29,7 @@ public class InitConfigService implements PostDeployVerticle {
       else{
         handler.handle(io.vertx.core.Future.failedFuture(reply.statusCode() + ", " + reply.statusMessage()));
       }
-    });*/
+    });
     handler.handle(io.vertx.core.Future.succeededFuture(true));
 
   }
