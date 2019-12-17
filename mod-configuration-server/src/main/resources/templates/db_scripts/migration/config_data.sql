2d4db0f8c3c7216808041c4001ba49b566ee0d73
Aliaksandr_Pautau <aliaksandr_pautau@epam.com>
2019-12-17 20:54:42 +0300

    MODCONF-36 Data migration script for changes to PO Number prefix/suffix entries
    - removing function

--- mod-configuration-server/src/main/resources/templates/db_scripts/migration/config_data.sql
+++ mod-configuration-server/src/main/resources/templates/db_scripts/migration/config_data.sql
@@ -1,22 +1,25 @@
 CREATE EXTENSION "uuid-ossp";
 
-CREATE OR REPLACE FUNCTION migrate_prefix_suffix(config_name TEXT) RETURNS void  AS $$
-BEGIN
-  INSERT INTO ${myuniversity}_${mymodule}.config_data
-  SELECT uuid_generate_v4(),
-    jsonb - 'id' || jsonb_build_object('value', jsonb_build_object('name', json_array_elements(cast(jsonb->>'value' as json)->'selectedItems'))::text)
-    || jsonb_build_object('configName', 'orders.' || config_name)
-    || jsonb_build_object('code',  EXTRACT(EPOCH FROM now())::text || GENERATE_SERIES(1, json_array_length(cast(jsonb->>'value' as json)->'selectedItems')))
-  FROM ${myuniversity}_${mymodule}.config_data
-  WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = config_name || 'es';
+INSERT INTO ${myuniversity}_${mymodule}.config_data
+SELECT uuid_generate_v4(),
+  jsonb - 'id' || jsonb_build_object('value', jsonb_build_object('name', json_array_elements(cast(jsonb->>'value' as json)->'selectedItems'))::text)
+  || jsonb_build_object('configName', 'orders.prefix')
+  || jsonb_build_object('code',  EXTRACT(EPOCH FROM now())::text || GENERATE_SERIES(1, json_array_length(cast(jsonb->>'value' as json)->'selectedItems')))
+FROM ${myuniversity}_${mymodule}.config_data
+WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'prefixes';
 
-  DELETE FROM ${myuniversity}_${mymodule}.config_data
-  WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = config_name || 'es';
-END;
-$$ LANGUAGE plpgsql
+DELETE FROM ${myuniversity}_${mymodule}.config_data
+WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'prefixes';
 
-SELECT migrate_prefix_suffix('prefix');
-SELECT migrate_prefix_suffix('suffix');
+INSERT INTO ${myuniversity}_${mymodule}.config_data
+SELECT uuid_generate_v4(),
+  jsonb - 'id' || jsonb_build_object('value', jsonb_build_object('name', json_array_elements(cast(jsonb->>'value' as json)->'selectedItems'))::text)
+  || jsonb_build_object('configName', 'orders.suffix')
+  || jsonb_build_object('code',  EXTRACT(EPOCH FROM now())::text || GENERATE_SERIES(1, json_array_length(cast(jsonb->>'value' as json)->'selectedItems')))
+FROM ${myuniversity}_${mymodule}.config_data
+WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'suffixes';
 
-DROP FUNCTION migrate;
-DROP EXTENSION IF EXISTS "uuid-ossp";
+DELETE FROM ${myuniversity}_${mymodule}.config_data
+WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'suffixes';
+
+DROP EXTENSION "uuid-ossp";
