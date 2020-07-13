777b33443b701c0971d728fff7a4da26dc9ca57e
Julian Ladisch <julianladisch@users.noreply.github.com>
2020-07-13 17:45:04 +0200

    MODCONF-54: Udate RMB to 30.2.4 fixing gin_trgm_ops schema
    Upgrade RMB from 30.0.2 to 30.2.4 fixing gin_trgm_ops schema:
    https://issues.folio.org/browse/MODORDSTOR-161
    
    Remove uuid-ossp extension that fails in pgPool environments:
    https://issues.folio.org/browse/FOLIO-2447
    Use md5(jsonb::text)::uuid to calculate an unique uuid.
    
    Let RMB automatically start embedded postgres.
    
    Remove unused PostgreSQL configuration from pom.xml.

--- mod-configuration-server/src/main/resources/templates/db_scripts/migration/config_data.sql
+++ mod-configuration-server/src/main/resources/templates/db_scripts/migration/config_data.sql
@@ -1,25 +1,29 @@
-CREATE EXTENSION "uuid-ossp";
-
 INSERT INTO ${myuniversity}_${mymodule}.config_data
-SELECT uuid_generate_v4(),
-  jsonb - 'id' || jsonb_build_object('value', jsonb_build_object('name', json_array_elements(cast(jsonb->>'value' as json)->'selectedItems'))::text)
-  || jsonb_build_object('configName', 'orders.prefix')
-  || jsonb_build_object('code',  EXTRACT(EPOCH FROM now())::text || GENERATE_SERIES(1, json_array_length(cast(jsonb->>'value' as json)->'selectedItems')))
-FROM ${myuniversity}_${mymodule}.config_data
-WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'prefixes';
+SELECT md5(jsonb::text)::uuid, jsonb
+FROM (
+  SELECT jsonb - 'id'
+    || jsonb_build_object('value', jsonb_build_object('name', json_array_elements(cast(jsonb->>'value' as json)->'selectedItems'))::text)
+    || jsonb_build_object('configName', 'orders.prefix')
+    || jsonb_build_object('code',  EXTRACT(EPOCH FROM now())::text || GENERATE_SERIES(1, json_array_length(cast(jsonb->>'value' as json)->'selectedItems')))
+    AS jsonb
+  FROM ${myuniversity}_${mymodule}.config_data
+  WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'prefixes'
+) x;
 
 DELETE FROM ${myuniversity}_${mymodule}.config_data
 WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'prefixes';
 
 INSERT INTO ${myuniversity}_${mymodule}.config_data
-SELECT uuid_generate_v4(),
-  jsonb - 'id' || jsonb_build_object('value', jsonb_build_object('name', json_array_elements(cast(jsonb->>'value' as json)->'selectedItems'))::text)
-  || jsonb_build_object('configName', 'orders.suffix')
-  || jsonb_build_object('code',  EXTRACT(EPOCH FROM now())::text || GENERATE_SERIES(1, json_array_length(cast(jsonb->>'value' as json)->'selectedItems')))
-FROM ${myuniversity}_${mymodule}.config_data
-WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'suffixes';
+SELECT md5(jsonb::text)::uuid, jsonb
+FROM (
+  SELECT jsonb - 'id'
+    || jsonb_build_object('value', jsonb_build_object('name', json_array_elements(cast(jsonb->>'value' as json)->'selectedItems'))::text)
+    || jsonb_build_object('configName', 'orders.suffix')
+    || jsonb_build_object('code',  EXTRACT(EPOCH FROM now())::text || GENERATE_SERIES(1, json_array_length(cast(jsonb->>'value' as json)->'selectedItems')))
+    AS jsonb
+  FROM ${myuniversity}_${mymodule}.config_data
+  WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'suffixes'
+) x;
 
 DELETE FROM ${myuniversity}_${mymodule}.config_data
 WHERE jsonb->>'module' = 'ORDERS' AND jsonb->>'configName' = 'suffixes';
-
-DROP EXTENSION "uuid-ossp";
